d834050b8878e467f50f8d4c3fc2610317d7cfff
wapalila <waspgift@gmail.com>
2026-09-25 21:11:05 +0300

    cleanup: remove unused KYC import

--- soko_langu/lib/screens/kyc/kyc_screen.dart
+++ soko_langu/lib/screens/kyc/kyc_screen.dart
@@ -4,7 +4,6 @@ import 'package:flutter/services.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:uuid/uuid.dart'; // ignore: unused_import
 import '../../services/kyc_service.dart';
 import '../../services/cloudinary_service.dart';
 import '../../widgets/google_loading.dart';
