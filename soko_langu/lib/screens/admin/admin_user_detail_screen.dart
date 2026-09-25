52c80242c9af9661853b4f56a04f30bb9a2f70b4
wapalila <waspgift@gmail.com>
2026-09-25 21:14:17 +0300

    cleanup: remove unused admin user import

--- soko_langu/lib/screens/admin/admin_user_detail_screen.dart
+++ soko_langu/lib/screens/admin/admin_user_detail_screen.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart'; // ignore: unused_import
 import 'package:go_router/go_router.dart';
 import 'package:http/http.dart' as http;
 import '../../extensions/context_tr.dart';
